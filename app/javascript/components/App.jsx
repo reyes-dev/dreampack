6a0fa491314bba2c597ee2f47991f9669618532a
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-21 03:13:35 -0700

    Add Entry component to App component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Entry from "./Entry";
 
 function App() {
     return (
         <main className='flex justify-center'>
             <h1>Lucid App</h1>
+            <Entry />
         </main>
     );
 };
