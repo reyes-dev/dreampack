7b3d901147a46ef97022b0b3a8f48ace540faced
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-20 23:24:56 -0700

    Set up React stimulus controller

--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
+import React from "react";
+import { createRoot } from 'react-dom/client';
 
 // Connects to data-controller="react"
 export default class extends Controller {
   connect() {
+    console.log('React controller connected.');
+    const app = document.getElementById('app');
+    createRoot(app).render(<App />);
   }
 }
