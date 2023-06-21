af6fe6201cc36fe65e543fa8222d1daed95b87de
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-20 23:31:39 -0700

    Set up initial App React component

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+    return <div>Lucid App</div>;
+};
+
+export default App;
--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import App from '../components/App';
 
 // Connects to data-controller="react"
 export default class extends Controller {
