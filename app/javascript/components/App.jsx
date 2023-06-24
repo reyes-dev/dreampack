505d51014e0c66cda60cfe28a193f80317b9b49d
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-24 02:39:35 -0700

    Setup initial draft css for journal/homepage, unresponsive

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -3,8 +3,7 @@ import Entry from "./Entry";
 
 function App() {
     return (
-        <main className='flex justify-center'>
-            <h1>Lucid App</h1>
+        <main className='flex justify-center w-full h-full'>
             <Entry />
         </main>
     );
--- app/javascript/components/Entry.jsx
+++ app/javascript/components/Entry.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function Entry() {
     return (
-        <section>
-            <input name="entryTitle" placeholder="Title" />
-            <textarea name="entryText" placeholder="Your entry here..." />
-            <button>Save Entry</button>
+        <section className='flex flex-col bg-white px-8 py-8 gap-4 w-[35%] max-w-4xl min-h-[550] max-h-[95%] shadow-2xl'>
+            <div className='flex'>
+                <input className='text-3xl flex-1 outline-none' name="entryTitle" placeholder="Entry Title" />
+                <button className='text-sky-500 self-start italic'>Save entry</button>
+            </div>
+                <textarea className='h-full outline-none resize-none' name="entryText" placeholder="Your entry here..." />
         </section>
     );
 };
