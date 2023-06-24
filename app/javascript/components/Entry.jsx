f0eccd6e17fd7867b07b73ec11b58fd6cfd71615
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-24 02:55:00 -0700

    (css) Add bottom borders for entry component

--- app/javascript/components/Entry.jsx
+++ app/javascript/components/Entry.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function Entry() {
     return (
         <section className='flex flex-col bg-white px-8 py-8 gap-4 w-[35%] max-w-4xl min-h-[550] max-h-[95%] shadow-2xl'>
-            <div className='flex'>
+            <div className='flex border-b pb-2'>
                 <input className='text-3xl flex-1 outline-none' name="entryTitle" placeholder="Entry Title" />
                 <button className='text-sky-500 self-start italic'>Save entry</button>
             </div>
             
-            <div>
+            <div className='border-b pb-2'>
                 <p className='text-gray-600'>Created on {new Date().toDateString()}</p>
             </div>
             
