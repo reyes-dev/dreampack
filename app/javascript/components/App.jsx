8dea357ed4a2999ebe942bd6f42b159c9ad3aaae
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-24 03:07:01 -0700

    (css) remove padding

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Entry from "./Entry";
-
+import Navbar from "./Navbar"
 function App() {
     return (
         <main className='flex justify-center w-full h-full'>
