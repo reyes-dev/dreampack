833c68b40b8f40025d8c4738382a53a55938ca8c
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-28 19:22:22 -0700

    (react) Add route in app for EditEntry component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import Entry from "./Entry";
 import EntryIndex from "./EntryIndex";
 import NewEntry from "./NewEntry";
+import EditEntry from "./EditEntry";
 import { Switch, Route } from "wouter";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Navbar /> 
             <div className='flex flex-col w-full h-full justify-center items-center'>
                 <Switch>
+                    <Route path="/entrys/index/:id/edit" component={EditEntry} />
                     <Route path="/entrys/index/:id" component={Entry} />
                     <Route path="/entrys/index" component={EntryIndex} />
                     <Route path='/' component={NewEntry} />
