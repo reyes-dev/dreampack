6d367f04b8f9c453adf35cc5394352ef2f91031b
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-28 19:12:34 -0700

    (react) Add link to create new entry form in navbar

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -3,8 +3,9 @@ import { Link } from "wouter";
 
 function Navbar() {
     return (
-        <nav className='self-start h-full w-[20%] bg-gray-900'>
+        <nav className='flex flex-col self-start h-full w-[20%] bg-gray-900'>
             <Link href="/entrys/index" className="text-white">All Journal Entries</Link>
+            <Link href="/" className="text-white">New Entry</Link>
         </nav>
     );
 };
