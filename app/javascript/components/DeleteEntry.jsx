ce0562d9a549921a5d08f3f1fca140fdde487542
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-30 00:48:57 -0700

    (react) Create DeleteEntry component and add it to Entry component

---
+++ app/javascript/components/DeleteEntry.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DeleteEntry() {
+    return (
+        <button className='text-rose-700'>
+            [ - ]
+        </button>
+    );
+};
+
+export default DeleteEntry;
--- app/javascript/components/Entry.jsx
+++ app/javascript/components/Entry.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "wouter";
+import DeleteEntry from "./DeleteEntry";
 
 function Entry({params}) {
     const [entry, setEntry] = useState({});
@@ -21,8 +22,11 @@ function Entry({params}) {
         max-w-4xl max-h-[90%] h-full shadow-2xl'>
             <div className='flex justify-between'>
                 <h1>{entry.title}</h1>
-                <Link href={`/entrys/index/${params.id}/edit`} 
+                <div className='flex gap-4'>
+                    <DeleteEntry />
+                    <Link href={`/entrys/index/${params.id}/edit`} 
                 className='text-sky-500 self-start italic'>Edit Entry</Link>
+                </div>
             </div>
             <p>{entry.created_at}</p>
             <p>{entry.body}</p>
