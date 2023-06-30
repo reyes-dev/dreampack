d3cb5852e0759446ab172668deb7dcd3d5839342
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-30 04:01:55 -0700

    (html/css) Add padding and underlines to navbar

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from "wouter";
 
 function Navbar() {
     return (
-        <nav className='flex flex-col self-start h-full w-[20%] bg-gray-900'>
-            <Link href="/entrys/index" className="text-white">All Journal Entries</Link>
-            <Link href="/" className="text-white">New Entry</Link>
+        <nav className='flex flex-col self-start h-full w-[15%] p-8 gap-2 bg-gray-900 underline-offset-4'>
+            <Link href="/entrys/index" className="text-white hover:underline">All Journal Entries</Link>
+            <Link href="/" className="text-white hover:underline">New Entry</Link>
         </nav>
     );
 };
