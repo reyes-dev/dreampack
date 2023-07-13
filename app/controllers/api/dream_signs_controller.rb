b1a26e4a2f308d7bf19bca2f417628ab26c210a1
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-07-12 17:08:49 -0700

    (rails) Add create dream_sign controller action

--- app/controllers/api/dream_signs_controller.rb
+++ app/controllers/api/dream_signs_controller.rb
@@ -1,2 +1,17 @@
 class DreamSignsController < ApplicationController
+  def create
+    dream_sign = DreamSign.create(dream_sign_params)
+
+    if dream_sign.save
+      render json: dream_sign
+    else
+      render json: dream_sign.errors
+    end
+  end
+ 
+  private
+
+  def dream_sign_params 
+    params.require(:dream_sign).permit(:phrase, :description)
+  end 
 end
