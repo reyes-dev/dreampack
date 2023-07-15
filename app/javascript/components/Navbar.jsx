66172a85db32dbc0da63ce5da29a79ef69f3f871
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-07-15 00:42:35 -0700

    (css) Add basic styling to Settings link and component

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
         <nav className='flex flex-col self-start h-full w-[15%] p-8 gap-2 bg-gray-900 underline-offset-4'>
             <Link href="/entrys/index" className="text-white hover:underline">All Journal Entries</Link>
             <Link href="/" className="text-white hover:underline">New Entry</Link>
-            <Link href="/settings">Settings</Link>
+            <Link href="/settings" className="text-white hover:underline">Settings</Link>
         </nav>
     );
 };
--- app/javascript/components/Settings.jsx
+++ app/javascript/components/Settings.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function Settings() {
     return (
-        <form>
+        <form className='flex flex-col bg-white px-8 py-8 gap-4 w-[35%] 
+        max-w-4xl max-h-[90%] h-full shadow-2xl'>
         </form>
     );
 };
