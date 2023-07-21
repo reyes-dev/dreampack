1c75431609df4b7fe84275f247fb2fd61e38be57
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-07-20 21:09:51 -0700

    (rails) Add resource for interpretation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  
+
   namespace :api do
-    resources :entries
-    resources :dream_signs
-  end 
+    resources :entries do
+      resource :interpretation
+    end
+    resources :dream_sign
+  end
 
   get '/*path' => 'pages#home'
 end
