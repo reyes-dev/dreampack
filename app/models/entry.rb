26137b9b0e4d34fd6e60839b434c3e556899021a
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-07-20 21:43:58 -0700

    (rails) Add create interpretation after entry creation

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,13 @@
 class Entry < ApplicationRecord
+  after_create :add_interpretation
   has_one :interpretation, dependent: :destroy
 
   validates :title, presence: true, length: { in: 1..250 }
   validates :body, presence: true
+
+  private
+
+  def add_interpretation
+    create_interpretation(body: '')
+  end
 end
