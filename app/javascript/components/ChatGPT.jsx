e9be51476aab6483825fbe2fd51fce0ab2997292
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-07-22 11:19:15 -0700

    (react) Add chatgpt dream interpretation component with logic for calling api

---
+++ app/javascript/components/ChatGPT.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function ChatGPT({ entry_id }) {
+  const createChatGPTResponse = async (event) => {
+    event.preventDefault();
+    const url = `/api/entries/${entry_id}/interpretation/chatgpt_response`;
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "X-CSRF-Token": token,
+          "Content-Type": "application/json",
+        },
+      });
+      await response.text();
+      return response.ok;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return <button onClick={createChatGPTResponse}>Interpret with AI</button>;
+}
+
+export default ChatGPT;
