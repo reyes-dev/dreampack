48f0eab8a30f529fb457e95a92c376fdc2d202fc
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-07-27 13:55:47 -0700

    (rails) Add attachment of DallE-2 response, a generated image, to Entry instance via Active Storage

--- app/controllers/api/dalle_responses_controller.rb
+++ app/controllers/api/dalle_responses_controller.rb
@@ -1,7 +1,11 @@
+require 'open-uri'
+
 class API::DalleResponsesController < ApplicationController
   before_action :open_client
+  after_action :grab_image
 
   def create
+    @entry = Entry.find(params[:entry_id])
     @prompt = params[:_json]
     @response = @client.images.generate(
       parameters: {
@@ -16,5 +20,9 @@ class API::DalleResponsesController < ApplicationController
 
   def open_client
     @client = OpenAI::Client.new
+  end
+
+  def grab_image
+    @entry.image.attach(io: URI.parse(@response).open, filename: 'whatever.png')
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,7 @@
 class Entry < ApplicationRecord
   after_create :add_interpretation
   has_one :interpretation, dependent: :destroy
+  has_one_attached :image
 
   validates :title, presence: true, length: { in: 1..250 }
   validates :body, presence: true
