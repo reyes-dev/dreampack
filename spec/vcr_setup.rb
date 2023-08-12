f95984085970b6e67da42875b50668e912e88602
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-08-12 12:14:01 -0700

    (rails/rspec) Prevent VCR from leaking sensitive API data

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -7,4 +7,9 @@ VCR.configure do |c|
   c.default_cassette_options = {
     serialize_with: :json
   }
+  c.filter_sensitive_data('<AUTH>') { |interaction| interaction.request.headers['Authorization'][0].sub('Bearer ', '') }
+  c.preserve_exact_body_bytes do |http_message|
+    http_message.body.encoding.name == 'ASCII-8BIT' ||
+      !http_message.body.valid_encoding?
+  end
 end
