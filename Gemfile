39d93c81a00f227a4c8bb70a5026f41e8bfd63aa
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-08-23 01:52:20 -0700

    (e2e testing) Set up Cypress

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'turbo-rails'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 group :development, :test do
+  gem 'cypress-on-rails', '~> 1.0'
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails'
   gem 'vcr', '~> 6.2'
---
+++ config/initializers/cypress_on_rails.rb
@@ -0,0 +1,19 @@
+if defined?(CypressOnRails)
+  CypressOnRails.configure do |c|
+    c.api_prefix = ""
+    c.install_folder = File.expand_path("#{__dir__}/../../e2e")
+    # WARNING!! CypressOnRails can execute arbitrary ruby code
+    # please use with extra caution if enabling on hosted servers or starting your local server on 0.0.0.0
+    c.use_middleware = !Rails.env.production?
+    #  c.use_vcr_middleware = !Rails.env.production?
+    c.logger = Rails.logger
+  end
+
+  # # if you compile your asssets on CI
+  # if ENV['CYPRESS'].present? && ENV['CI'].present?
+  #  Rails.application.configure do
+  #    config.assets.compile = false
+  #    config.assets.unknown_asset_fallback = false
+  #  end
+  # end
+end
