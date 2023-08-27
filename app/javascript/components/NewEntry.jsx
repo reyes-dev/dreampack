ca344f0d7274a2a0de6953e4317f67c0f7583fca
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-08-27 09:25:36 -0700

    (html/css) Upgrade styling of New Entry form

--- app/javascript/components/NewEntry.jsx
+++ app/javascript/components/NewEntry.jsx
@@ -53,37 +53,43 @@ function NewEntry() {
   return (
     <form
       ref={formRef}
-      className="flex h-full max-h-[90%] w-[35%] max-w-4xl flex-col gap-4 
-        bg-white px-8 py-8 shadow-2xl"
+      className="flex h-[80vh] w-1/2 flex-col gap-4 rounded
+         border-2 border-dashed border-[hsl(133.1,66.1%,76.9%)] bg-[#08041A] p-8"
       onSubmit={createEntry}
     >
-      <div className="flex border-b pb-2">
+      <div className="flex justify-between gap-4 border-b pb-2">
         <input
-          className="flex-1 text-3xl outline-none"
+          className="w-full bg-transparent text-3xl outline-none"
           name="entryTitle"
           data-cy="entryTitle"
           onChange={(event) => onChange(event, setTitle)}
           placeholder="Entry Title"
         />
-        <button type="submit" className="self-start italic text-sky-500">
+        <button
+          type="submit"
+          className="text-md min-h whitespace-nowrap rounded 
+                      border border-sky-500 p-[0.450rem_0.450rem_0.4625rem] 
+                      italic text-sky-500 hover:bg-slate-700 "
+        >
           Save entry
         </button>
       </div>
 
-      <div className="border-b pb-2">
-        <p className="text-gray-600">Created on {new Date().toDateString()}</p>
+      <div className="flex items-center justify-between border-b pb-2">
+        <p className=" text-gray-600">Created on {new Date().toDateString()}</p>
+        <div className="flex gap-4 pb-2">
+          <Whisper setEntryBodyHandler={setEntryBodyHandler} />
+        </div>
       </div>
 
       <textarea
-        className="h-full resize-none outline-none"
+        className="h-full resize-none bg-transparent outline-none"
         name="entryText"
         data-cy="entryText"
         onChange={(event) => onChange(event, setBody)}
         placeholder="Your entry here..."
         value={body}
       />
-
-      <Whisper setEntryBodyHandler={setEntryBodyHandler} />
     </form>
   );
 }
