2e24995cf6ed553adddc16294ed203ad00e812a5
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-05 19:47:13 -0700

    (rails) Update routes for Devise users and auth-related controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  get 'private/test'
+  get '/current_user', to: 'current_user#index'
   root 'pages#home'
+  devise_for :users, path: '', path_names: {
+                                 sign_in: 'login',
+                                 sign_out: 'logout',
+                                 registration: 'signup'
+                               },
+                     controllers: {
+                       sessions: 'users/sessions',
+                       registrations: 'users/registrations'
+                     }
 
   namespace :api do
     resources :entries do
@@ -11,7 +22,6 @@ Rails.application.routes.draw do
     resources :dream_signs
     resources :whisper_transcriptions, only: [:create]
   end
-
   get '*all', to: 'pages#home', constraints: lambda { |req|
                                                req.path.exclude? 'rails/active_storage'
                                              }
