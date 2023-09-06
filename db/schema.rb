bc33ff86f9d5ee102c083e0eda5162968dc0777d
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-05 19:47:30 -0700

    (rails) Update schema, users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_183149) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_232903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_183149) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_dream_signs_on_user_id"
   end
 
   create_table "entries", force: :cascade do |t|
@@ -54,6 +56,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_183149) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
   create_table "interpretations", force: :cascade do |t|
@@ -64,7 +68,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_183149) do
     t.index ["entry_id"], name: "index_interpretations_on_entry_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "jti", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "dream_signs", "users"
+  add_foreign_key "entries", "users"
   add_foreign_key "interpretations", "entries"
 end
