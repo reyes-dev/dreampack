fc887c8da29b8e99721f451f2f12abcdd13133a3
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-05 19:12:55 -0700

    (rails) Set up CORS initializer

---
+++ config/initializers/cors.rb
@@ -0,0 +1,10 @@
+# config/initializers/cors.rb
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*' # later change to the domain of the frontend app
+    resource '*',
+             headers: :any,
+             methods: %i[get patch put delete post options show],
+             expose: [:Authorization]
+  end
+end
