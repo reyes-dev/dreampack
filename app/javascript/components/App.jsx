0aab06db57e6c3189cce596fca3f37225142c1cd
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-09 00:19:45 -0700

    (react) Modify App component to be wrapped in Context Provider object

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Homepage from "./About/Homepage";
 import Navbar from "./Navbar/Navbar";
 import Sidebar from "./Sidebar/Sidebar";
@@ -10,37 +10,60 @@ import Settings from "./Settings/Settings";
 import { Switch, Route } from "wouter";
 import Interpretation from "./Interpretation/ShowInterpretation/Interpretation";
 import EditInterpretation from "./Interpretation/EditInterpretation/EditInterpretation";
-import SignUp from "./Registrations/SignUp";
-import SignIn from "./Sessions/SignIn";
+import { UserContext } from "../context/UserContext";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // Check logged in status to conditionally render components
+  // Look into protected routes
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  const getCurrentUser = async () => {
+    const url = `/current_user`;
+    try {
+      const response = await fetch(url);
+      if (response.status === 204) return;
+      const data = await response.json();
+      toggleLoggedIn();
+      return data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const toggleLoggedIn = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
+
   return (
-    <main className="flex h-screen text-white">
-      <Sidebar />
-      <div className="flex w-full flex-col-reverse justify-end">
-        <section className="flex h-full flex-col items-center overflow-hidden pb-8 pl-8 pr-8">
-          <Switch>
-            <Route path="/" component={Homepage} />
-            <Route path="/entries/new" component={NewEntry} />
-            <Route path="/entries/:id/edit" component={EditEntry} />
-            <Route path="/entries/:id" component={Entry} />
-            <Route path="/entries" component={EntryIndex} />
-            <Route path="/settings" component={Settings} />
-            <Route
-              path="/entries/:id/interpretation"
-              component={Interpretation}
-            />
-            <Route
-              path="/entries/:id/interpretation/edit"
-              component={EditInterpretation}
-            />
-            <Route path="/sign_up" component={SignUp} />
-            <Route path="/sign_in" component={SignIn} />
-          </Switch>
-        </section>
-        <Navbar />
-      </div>
-    </main>
+    <UserContext.Provider value={{ isLoggedIn, toggleLoggedIn }}>
+      <main className="flex h-screen text-white">
+        {isLoggedIn ? <Sidebar /> : isLoggedIn}
+        <div className="flex w-full flex-col-reverse justify-end">
+          <section className="flex h-full flex-col items-center overflow-hidden pb-8 pl-8 pr-8">
+            <Switch>
+              <Route path="/" component={Homepage} />
+              <Route path="/entries/new" component={NewEntry} />
+              <Route path="/entries/:id/edit" component={EditEntry} />
+              <Route path="/entries/:id" component={Entry} />
+              <Route path="/entries" component={EntryIndex} />
+              <Route path="/settings" component={Settings} />
+              <Route
+                path="/entries/:id/interpretation"
+                component={Interpretation}
+              />
+              <Route
+                path="/entries/:id/interpretation/edit"
+                component={EditInterpretation}
+              />
+            </Switch>
+          </section>
+          <Navbar />
+        </div>
+      </main>
+    </UserContext.Provider>
   );
 }
 
