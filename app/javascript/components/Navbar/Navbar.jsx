b925ded7d7ef353638de9c8e171986d0da16a9c6
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-09 00:17:46 -0700

    (react) Conditionally render login link depending on logged-in status

--- app/javascript/components/Navbar/Navbar.jsx
+++ app/javascript/components/Navbar/Navbar.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import NavbarLink from "./NavbarLink";
+import { UserContext } from "../../context/UserContext";
 
 function Navbar() {
+  const { isLoggedIn } = useContext(UserContext);
   return (
     <nav className="flex h-fit items-center justify-end gap-8 p-4 xl:p-8">
       <NavbarLink
         destination="/"
         content="About"
-        bgColor="bg-emerald-900/20"
-        bgColorHover="hover:bg-emerald-900"
+        bgColor="bg-green-900/20"
+        bgColorHover="hover:bg-green-900"
       />
-      <NavbarLink
-        destination="/sign_in"
-        content="Sign In"
-        bgColor="bg-rose-900/20"
-        bgColorHover="hover:bg-rose-900"
-      />
+      {isLoggedIn ? null : (
+        <a
+          href="/login"
+          className="hover:.!text-[#09073a] hover:.bg-white hover:.outline-white 
+    .outline hover:.outline-offset-4 active:.outline-offset-2 flex w-fit 
+    min-w-max basis-1  items-center justify-center 
+     gap-2 rounded-full bg-blue-900/20 p-2 px-5 text-center text-sm  
+    font-bold text-yellow-50 outline-[#09073a]/50 backdrop-blur-sm 
+    transition-all hover:bg-blue-900 hover:bg-opacity-100 active:bg-opacity-90 
+    md:text-base"
+        >
+          Log In
+        </a>
+      )}
     </nav>
   );
 }
