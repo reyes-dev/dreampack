db09c8484bc9f29cbeeafe3744de069d38ca663c
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-11 16:36:19 -0700

    (rails) Update registration route pathname

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
                                  sign_in: 'login',
                                  sign_out: 'logout',
-                                 registration: ''
+                                 registration: '/registration'
                                },
                      controllers: {
                        sessions: 'users/sessions',
