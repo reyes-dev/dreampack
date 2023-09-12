20ca20d0863991a76b92069ddc7b35e6388f6cd3
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-12 16:39:13 -0700

    Refactor DallE-2 image generation button and function into separate component

---
+++ app/javascript/components/Entry/ShowEntry/DALLE2.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { FaPalette } from "react-icons/fa";
+
+function DALLE2({ entry_id, setDalleUrl, entry_body_text }) {
+  const generateImage = async () => {
+    const url = `/api/entries/${entry_id}/dalle_responses`;
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    const prompt = `Create an image based on the following dream: ${entry_body_text}`;
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "X-CSRF-Token": token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(prompt),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+      }
+      const data = await response.text();
+      if (data === null || data.trim() === "") {
+        return console.log(
+          "You received an empty response, meaning something is wrong. Please check to see if you entered the correct API key.",
+        );
+      }
+      setDalleUrl(data);
+      return data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <button
+      onClick={generateImage}
+      className="min-h flex items-center gap-2 whitespace-nowrap rounded border border-amber-500 
+                      p-[0.450rem_0.450rem_0.4625rem] text-sm italic 
+                      text-amber-500 hover:bg-slate-700 lg:text-lg"
+    >
+      <span className="lg:text-md hidden lg:block xl:text-lg">
+        Generate Image
+      </span>
+      <FaPalette />
+    </button>
+  );
+}
+
+export default DALLE2;
--- app/javascript/components/Entry/ShowEntry/Entry.jsx
+++ app/javascript/components/Entry/ShowEntry/Entry.jsx
@@ -4,8 +4,9 @@ import DeleteEntryButton from "./DeleteEntryButton";
 import * as Selection from "selection-popover";
 import DreamSign from "./DreamSign";
 import Highlighter from "react-highlight-words";
-import { FaEdit, FaPalette } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 import DeleteEntryModal from "./DeleteEntryModal";
+import DALLE2 from "./DALLE2";
 
 function Entry({ params }) {
   const [entry, setEntry] = useState({});
@@ -64,35 +65,6 @@ function Entry({ params }) {
     }
   };
 
-  const generateImage = async () => {
-    const url = `/api/entries/${params.id}/dalle_responses`;
-    const token = document.querySelector('meta[name="csrf-token"]').content;
-    const prompt = `Create an image based on the following dream: ${entry.body}`;
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "X-CSRF-Token": token,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(prompt),
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP Error! Status: ${response.status}`);
-      }
-      const data = await response.text();
-      if (data === null || data.trim() === "") {
-        return console.log(
-          "You received an empty response, meaning something is wrong. Please check to see if you entered the correct API key.",
-        );
-      }
-      setDalleUrl(data);
-      return data;
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   const handleMouseUp = () => {
     setSelectedText(window.getSelection().toString());
   };
@@ -146,17 +118,11 @@ function Entry({ params }) {
           >
             Go to Interpretation
           </Link>
-          <button
-            onClick={generateImage}
-            className="min-h flex items-center gap-2 whitespace-nowrap rounded border border-amber-500 
-                      p-[0.450rem_0.450rem_0.4625rem] text-sm italic 
-                      text-amber-500 hover:bg-slate-700 lg:text-lg"
-          >
-            <span className="lg:text-md hidden lg:block xl:text-lg">
-              Generate Image
-            </span>
-            <FaPalette />
-          </button>
+          <DALLE2
+            entry_id={params.id}
+            setDalleUrl={setDalleUrl}
+            entry_body_text={entry.body}
+          />
         </div>
       </div>
       <Selection.Root>
