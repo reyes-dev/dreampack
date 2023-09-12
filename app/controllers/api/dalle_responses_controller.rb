b6160870801955faac9a1123e2c46d88c6cf5868
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-12 14:29:52 -0700

    (rails) Modify DalleResponses controller to check for invalid api token

--- app/controllers/api/dalle_responses_controller.rb
+++ app/controllers/api/dalle_responses_controller.rb
@@ -3,23 +3,28 @@ require 'open-uri'
 class API::DalleResponsesController < ApplicationController
   before_action :authenticate_user!
   before_action :open_client
-  after_action :grab_image
+  after_action :grab_image, unless: -> { @empty_token || @response.nil? }
 
   def create
     @entry = Entry.find(params[:entry_id])
     @prompt = params[:_json]
-    @response = @client.images.generate(
-      parameters: {
-        prompt: @prompt,
-        size: '256x256'
-      }
-    ).dig('data', 0, 'url')
+    unless @client.nil?
+      @response = @client.images.generate(
+        parameters: {
+          prompt: @prompt,
+          size: '256x256'
+        }
+      ).dig('data', 0, 'url')
+    end
     render plain: @response
   end
 
   private
 
   def open_client
+    @empty_token = current_user.openai_token.nil? || current_user.openai_token.empty?
+    return if @empty_token
+
     @client = OpenAI::Client.new(access_token: current_user.openai_token)
   end
 
