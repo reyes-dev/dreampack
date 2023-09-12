d8795bbdb55d29196285ad78f1e803cc2b74e19a
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-12 14:30:25 -0700

    (rails) Modify WhisperTranscriptions controller to check for invalid api token and render correct response

--- app/controllers/api/whisper_transcriptions_controller.rb
+++ app/controllers/api/whisper_transcriptions_controller.rb
@@ -4,19 +4,25 @@ class API::WhisperTranscriptionsController < ApplicationController
 
   def create
     audio = params[:audio_data].tempfile.path.to_s
-    response = @client.transcribe(
-      parameters: {
-        model: 'whisper-1',
-        file: File.open(audio, 'rb')
+    unless @client.nil?
+      @response = @client.transcribe(
+        parameters: {
+          model: 'whisper-1',
+          file: File.open(audio, 'rb')
 
-      }
-    )
-    render plain: response['text']
+        }
+      )
+    end
+    @response = @response['text'] unless @response.nil?
+    render plain: @response
   end
 
   private
 
   def open_client
+    @empty_token = current_user.openai_token.nil? || current_user.openai_token.empty?
+    return if @empty_token
+
     @client = OpenAI::Client.new(access_token: current_user.openai_token)
   end
 end
