e4e76a56986fbf252a1c8dd30e0b8d1a6bf40087
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-12 13:03:02 -0700

    (rails) Modify ChatGPTResponses controller to check for invalid access token

--- app/controllers/api/chatgpt_responses_controller.rb
+++ app/controllers/api/chatgpt_responses_controller.rb
@@ -1,25 +1,30 @@
 class API::ChatGPTResponsesController < ApplicationController
   before_action :authenticate_user!
   before_action :open_client
-  after_action :update_interpretation
+  after_action :update_interpretation, unless: -> { @empty_token || @response.nil? }
 
   def create
     @entry = Entry.find(params[:entry_id])
     prompt = "Without giving a preface or introduction of yourself or your
               capabilities, please interpret the following dream: #{@entry.body}"
-    @response = @client.chat(
-      parameters: {
-        model: 'gpt-3.5-turbo', # Required.
-        messages: [{ role: 'user', content: prompt }], # Required.
-        temperature: 0.7
-      }
-    ).dig('choices', 0, 'message', 'content')
+    unless @client.nil?
+      @response = @client.chat(
+        parameters: {
+          model: 'gpt-3.5-turbo', # Required.
+          messages: [{ role: 'user', content: prompt }], # Required.
+          temperature: 0.7
+        }
+      ).dig('choices', 0, 'message', 'content')
+    end
     render plain: @response
   end
 
   private
 
   def open_client
+    @empty_token = current_user.openai_token.nil? || current_user.openai_token.empty?
+    return if @empty_token
+
     @client = OpenAI::Client.new(access_token: current_user.openai_token)
   end
 
