743dbacbd6663c831e18f78702ae6a3b9397c61d
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-12 22:19:25 -0700

    (rails) Update dream_signs controller to retrieve and create dream signs for currently logged in users

--- app/controllers/api/dream_signs_controller.rb
+++ app/controllers/api/dream_signs_controller.rb
@@ -2,14 +2,14 @@ class API::DreamSignsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    dream_signs = DreamSign.phrases
+    dream_signs = current_user.dream_signs.phrases
     render json: dream_signs.to_json
   end
 
   def create
-    dream_sign = DreamSign.create(dream_sign_params)
+    dream_sign = current_user.dream_signs.create(dream_sign_params)
 
-    if dream_sign.save
+    if dream_sign.save!
       render json: dream_sign, status: :created
     else
       render json: dream_sign.errors
