6195ce886e8bd58a2b08c2d5d16d6ee923bc72d1
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-19 13:09:37 -0700

    (accessibility) Add meaningful text to DeleteEntryButton, improve styling

--- app/javascript/components/Entry/ShowEntry/DeleteEntryButton.jsx
+++ app/javascript/components/Entry/ShowEntry/DeleteEntryButton.jsx
@@ -4,10 +4,11 @@ import { FaTrashAlt } from "react-icons/fa";
 function DeleteEntryButton({ toggleModalActivation }) {
   return (
     <button
-      className="rounded border border-rose-700 p-[0.450rem_0.450rem_0.4625rem] text-xs text-rose-700 hover:bg-slate-700 lg:text-lg"
+      className="flex items-center gap-2 whitespace-nowrap rounded border border-rose-700 p-[0.450rem_0.450rem_0.4625rem] text-xs text-rose-700 hover:bg-slate-700 lg:text-lg"
       onClick={toggleModalActivation}
       data-cy="deleteEntry"
     >
+      Delete Entry
       <FaTrashAlt />
     </button>
   );
