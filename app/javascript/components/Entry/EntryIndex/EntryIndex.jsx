bc5deb4251122c7ef6913a5c7d6cf94dfe9e9da9
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-09-18 19:29:54 -0700

    (accessibility) Reorganize HTML in EntryIndex component to be a section of unordered lists

--- app/javascript/components/Entry/EntryIndex/EntryIndex.jsx
+++ app/javascript/components/Entry/EntryIndex/EntryIndex.jsx
@@ -30,22 +30,21 @@ function EntryIndex() {
 
   const entryList = entries.map((entry) => {
     return (
-      <Link href={`/entries/${entry[0]}`} key={entry[0]}>
-        <ul className="sm:text-md cursor-pointer  whitespace-pre-line pt-2 text-sm hover:rounded hover:bg-gray-600 md:text-lg lg:pr-2 lg:text-xl">
-          <div className="flex justify-between pb-1">
-            <div className="flex-col items-baseline gap-2  lg:flex">
-              <h1 className="border-b">{entry[1]}</h1>
-
-              <p className="text-xs text-gray-400 lg:text-lg">
-                Created on {entry[3]}
-              </p>
-            </div>
-          </div>
-          <li className="pt md:text-md text-sm text-gray-200 lg:text-lg">
+      <li className="flex cursor-pointer flex-col justify-between gap-2 hover:rounded hover:bg-gray-600">
+        <Link
+          href={`/entries/${entry[0]}`}
+          key={entry[0]}
+          className="flex flex-col gap-2"
+        >
+          <h1 className="border-b">{entry[1]}</h1>
+          <p className="text-xs text-gray-400 lg:text-lg">
+            Created on <time>{entry[3]}</time>
+          </p>
+          <article className="pt md:text-md text-sm text-gray-200 lg:text-lg">
             {entry[2]}
-          </li>
-        </ul>
-      </Link>
+          </article>
+        </Link>
+      </li>
     );
   });
 
@@ -54,7 +53,13 @@ function EntryIndex() {
       className="flex h-full w-full flex-col gap-4 overflow-auto rounded 
     border-2 border-[hsl(133.1,66.1%,76.9%)] bg-[hsla(0,0%,0%,0.15)] p-8 lg:h-[80vh] xl:w-1/2"
     >
-      {entries.length ? entryList : <ZeroEntriesMessage />}
+      {entries.length ? (
+        <ul className="sm:text-md flex flex-col gap-4  whitespace-pre-line pt-2 text-sm  md:text-lg lg:pr-2 lg:text-xl">
+          {entryList}
+        </ul>
+      ) : (
+        <ZeroEntriesMessage />
+      )}
     </section>
   );
 }
