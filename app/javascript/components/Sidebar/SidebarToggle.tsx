2e62c8eeac13a6d1f4a812bcf61af0e9502e13e9
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-10-19 21:42:05 -0700

    Type props, remove unneeded href attribute

--- app/javascript/components/Sidebar/SidebarToggle.tsx
+++ app/javascript/components/Sidebar/SidebarToggle.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import { IconContext } from "react-icons";
 
-function SidebarToggle({ hideSidebar, icon, visible }) {
+interface SidebarToggleProps {
+  hideSidebar: () => void;
+  icon: ReactElement;
+  visible: boolean;
+}
+
+function SidebarToggle({ hideSidebar, icon, visible }: SidebarToggleProps) {
   return (
     <button
-      href="/"
       className={`${
         visible ? "h-full" : "h-fit"
       } hidden w-min items-center justify-center gap-3 rounded-md border border-white/20 p-3 transition-transform duration-300 ease-in hover:bg-slate-700 md:flex lg:text-[1.3em] 2xl:text-[1.6em]`}
