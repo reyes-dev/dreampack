6545edbf4f9e4f7b3907a089627c78ed5f0f65fe
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-10-19 21:33:00 -0700

    Type props

--- app/javascript/components/Entry/ShowEntry/DeleteEntryButton.tsx
+++ app/javascript/components/Entry/ShowEntry/DeleteEntryButton.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { FaTrashAlt } from "react-icons/fa";
 
-function DeleteEntryButton({ toggleModalActivation }) {
+interface DeleteEntryButtonProps {
+  toggleModalActivation: () => void;
+}
+
+function DeleteEntryButton({ toggleModalActivation }: DeleteEntryButtonProps) {
   return (
     <button
       className="flex items-center gap-2 whitespace-nowrap rounded border border-rose-700 p-[0.450rem_0.450rem_0.4625rem] text-xs text-rose-700 hover:bg-slate-700 lg:text-lg"
