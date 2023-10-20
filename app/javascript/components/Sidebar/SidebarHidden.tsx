d1abcd192d7de0c3f61c19c8fa5c88959e3472ce
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-10-19 21:41:43 -0700

    Type props

--- app/javascript/components/Sidebar/SidebarHidden.tsx
+++ app/javascript/components/Sidebar/SidebarHidden.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import SidebarToggle from "components/Sidebar/SidebarToggle";
 
-function SidebarHidden({ hideSidebar, icon, visible }) {
+interface SidebarHiddenProps {
+  hideSidebar: () => void;
+  icon: ReactElement;
+  visible: boolean;
+}
+
+function SidebarHidden({ hideSidebar, icon, visible }: SidebarHiddenProps) {
   return (
     <section className="flex h-screen flex-col gap-4  p-4 underline-offset-4">
       <SidebarToggle hideSidebar={hideSidebar} icon={icon} visible={visible} />
