7f13cf83e083fee4a8a51e4282a08b9a20c385b2
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-10-27 01:32:33 -0700

    update cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # later change to the domain of the frontend app
+    origins 'https://dreampack.app/' # later change to the domain of the frontend app
     resource '*',
              headers: :any,
              methods: %i[get patch put delete post options show],
