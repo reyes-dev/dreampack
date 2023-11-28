cb830a769c33c10e24010240e28510630932d06b
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-11-27 17:01:32 -0800

    (react) Remove sidebar toggle/hide stuff

--- app/javascript/components/Sidebar/Sidebar.tsx
+++ app/javascript/components/Sidebar/Sidebar.tsx
@@ -1,39 +1,79 @@
-import React, { useState } from "react";
-import SidebarVisible from "components/Sidebar/SidebarVisible";
-import SidebarHidden from "components/Sidebar/SidebarHidden";
-import { BsLayoutSidebarInsetReverse } from "react-icons/bs";
-import { BsLayoutSidebarInset } from "react-icons/bs";
+import React, { useState, useEffect, useContext } from "react";
+import SidebarLink from "components/Sidebar/SidebarLink";
+import LogOutButton from "components/Sidebar/LogOutButton";
+import { FaPlus, FaBook, FaCog, FaRegCommentAlt } from "react-icons/fa";
+import { SidebarEntryContext } from "context/SidebarEntryContext";
+/* Componentize the three "sections" into separate components */
+/* Use react-query, delete contexts */
+/* in-place map instead of variable map */
 
-function Sidebar() {
-  const [visible, setVisible] = useState(true);
+export default function Sidebar() {
+  const [sidebarEntries, setSidebarEntries] = useState([]);
+  const { sidebarEntriesUpdateTrigger } = useContext(SidebarEntryContext);
 
-  const hideSidebar = () => {
-    setVisible(!visible);
+  useEffect(() => {
+    getSidebarEntries();
+  }, [sidebarEntriesUpdateTrigger]);
+
+  const getSidebarEntries = async () => {
+    const url = `/api/sidebar_entry_links`;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      return setSidebarEntries(data);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
+  const sidebarEntriesList = sidebarEntries.map((entrySidebarLink) => {
+    return (
+      <li key={entrySidebarLink[0]}>
+        <SidebarLink
+          destination={`/entries/${entrySidebarLink[0]}`}
+          content={entrySidebarLink[1]}
+          icon={<FaRegCommentAlt />}
+        />
+      </li>
+    );
+  });
+
   return (
-    <nav
-      className={
-        visible
-          ? "sticky top-0 z-[1] col-start-1 inline-block w-full self-start sm:top-5 sm:w-fit"
-          : "absolute w-[5vw]"
-      }
-    >
-      {visible ? (
-        <SidebarVisible
-          hideSidebar={hideSidebar}
-          icon={<BsLayoutSidebarInsetReverse />}
-          visible={visible}
+    <nav className="sticky top-0 z-[1] col-start-1 w-full justify-between self-start bg-[hsl(237.1,25.9%,15.9%)] py-4 underline-offset-4 sm:top-5  sm:flex sm:w-fit sm:flex-col sm:gap-12 sm:p-4">
+      <section className="flex flex-col gap-12">
+        <div className="flex flex-col">
+          <h1 className="hidden w-full font-bold sm:block">Dream Journal</h1>
+          <div className="flex gap-2 sm:flex-col sm:pt-4">
+            <SidebarLink
+              destination="/entries/new"
+              content="New Entry"
+              icon={<FaPlus />}
+            />
+            <SidebarLink
+              destination="/entries/page/0"
+              content="All Entries"
+              icon={<FaBook />}
+            />
+          </div>
+        </div>
+        <section className="hidden flex-col md:flex">
+          <h1 className="hidden font-bold sm:block">Recent Entries</h1>
+          <ul className="hidden flex-col gap-2 pt-4 md:flex" role="list">
+            {sidebarEntriesList}
+          </ul>
+        </section>
+      </section>
+      <section className="flex items-center gap-2 md:flex-col">
+        <SidebarLink
+          destination="/settings"
+          content="Settings"
+          icon={<FaCog />}
         />
-      ) : (
-        <SidebarHidden
-          hideSidebar={hideSidebar}
-          icon={<BsLayoutSidebarInset />}
-          visible={visible}
-        />
-      )}
+        <LogOutButton />
+      </section>
     </nav>
   );
 }
-
-export default Sidebar;
