d132f86b13af5e9649dc56eae07e5238fa56e6a6
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-12-20 01:03:29 -0800

    (react) Repurpose Pagination component to work with TanStack Query

--- app/javascript/components/Entry/EntryIndex/EntryIndex.tsx
+++ app/javascript/components/Entry/EntryIndex/EntryIndex.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { Link, useRoute } from "wouter";
+import Pagination from "components/Shared/Pagination";
 
 function EntryIndex() {
   const [, params] = useRoute("/entries/page/:page");
@@ -53,32 +54,12 @@ function EntryIndex() {
           );
         })}
       </ul>
-      <nav className="flex w-full items-center justify-between self-center pt-8">
-        <button
-          onClick={() => setPage((old) => Math.max(old - 1, 0))}
-          disabled={page === 0}
-          className={`${
-            page === 0 ? "invisible" : ""
-          } rounded-md border p-[10px_20px] hover:border-sky-500 hover:bg-gray-700`}
-        >
-          Previous Page
-        </button>{" "}
-        <span>{page}</span>
-        <button
-          onClick={() => {
-            if (!isPlaceholderData && hasMore) {
-              setPage((old) => old + 1);
-            }
-          }}
-          // Disable the Next Page button until we know a next page is available
-          disabled={isPlaceholderData || !hasMore}
-          className={`${
-            isPlaceholderData || !hasMore ? "invisible" : ""
-          } rounded-md border p-[10px_20px] hover:border-sky-500 hover:bg-gray-700`}
-        >
-          Next Page
-        </button>
-      </nav>
+      <Pagination
+        page={page}
+        setPage={setPage}
+        hasMore={hasMore}
+        isPlaceholderData={isPlaceholderData}
+      />
       {isFetching ? <span> Loading...</span> : null}{" "}
     </section>
   );
--- app/javascript/components/Shared/Pagination.tsx
+++ app/javascript/components/Shared/Pagination.tsx
@@ -1,44 +1,48 @@
-import React, { useMemo } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { useLocation } from "wouter";
 
 interface PaginationProps {
-  pageNumber: number;
-  entryCount: number;
+  page: number;
+  setPage: Dispatch<SetStateAction<number>>;
+  hasMore: boolean;
+  isPlaceholderData: boolean;
 }
 
-function Pagination({ pageNumber, entryCount }: PaginationProps) {
+function Pagination({
+  page,
+  setPage,
+  hasMore,
+  isPlaceholderData,
+}: PaginationProps) {
   const [, navigate] = useLocation();
 
-  const pageCount = useMemo(() => {
-    return Math.floor(entryCount / 10);
-  }, [entryCount]);
-
   return (
-    <section className="flex justify-center gap-8 self-center pt-2 sm:pt-8">
-      {pageNumber === 0 ? null : (
-        <button
-          className="pb pt rounded border border-white/20 pl-4 pr-4 hover:border-white hover:bg-slate-700"
-          onClick={() => {
-            navigate(`/entries/page/${pageNumber - 1}`);
-          }}
-        >
-          {"<-"}
-        </button>
-      )}
-
-      <p className="self-end border-b-2 text-lg">{pageNumber}</p>
-
-      {pageNumber === pageCount ? null : (
-        <button
-          className="pb pt rounded border border-white/20 pl-4 pr-4 hover:border-white hover:bg-slate-700"
-          onClick={() => {
-            navigate(`/entries/page/${pageNumber + 1}`);
-          }}
-        >
-          {"->"}
-        </button>
-      )}
-    </section>
+    <nav className="flex w-full items-center justify-between self-center pt-8">
+      <button
+        onClick={() => setPage((old: number) => Math.max(old - 1, 0))}
+        disabled={page === 0}
+        className={`${
+          page === 0 ? "invisible" : ""
+        } rounded-md border p-[10px_20px] hover:border-sky-500 hover:bg-gray-700`}
+      >
+        Previous Page
+      </button>{" "}
+      <span>{page}</span>
+      <button
+        onClick={() => {
+          if (!isPlaceholderData && hasMore) {
+            setPage((old) => old + 1);
+          }
+        }}
+        // Disable the Next Page button until we know a next page is available
+        disabled={isPlaceholderData || !hasMore}
+        className={`${
+          isPlaceholderData || !hasMore ? "invisible" : ""
+        } rounded-md border p-[10px_20px] hover:border-sky-500 hover:bg-gray-700`}
+      >
+        Next Page
+      </button>
+    </nav>
   );
 }
 
